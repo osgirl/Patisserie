f7281e0297fa830c42f46baf018b8a695a2e4f36
Obert Guo <obertguo@gmail.com>
2019-01-26 01:49:07 -0500

    Omfg, this new db and json files, etc, etc, is way better with the gacha cmd. Also, I forgot to rename sample-copy.js to help.js LMAO

---
+++ commands/createColorRoles.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+module.exports = {
+    name: 'createColorRoles',
+    description: 'Writes allowed color roles using ./Roles/serverRoles.json, to ./Roles/colorRoles.json' ,
+    execute(message, args) {
+
+        /*
+        var role = message.guild.roles.find('name' , 'Blueberry Mousse');
+
+        var memberRoles = {
+                [message.author.id]: role.name  
+            };
+
+        console.log(memberRoles);*/
+
+        if(message.author.id !== '226457061959925761') return;
+
+
+        fs.readFile('./Roles/serverRoles.json', (err, data) =>{
+            if(err) throw err;
+
+            const serverRoles = JSON.parse(data);
+            //Retrieve allowed color roles by key from serverRoles.json
+            var validColorRoles = ['1','3','4','5','6','8','11','12','13','15','17','18','21','23','24','28','31','32','33','35','36','37','38','39','40'];
+            
+            validColorRoles = Object.keys(serverRoles)
+             .filter(key => validColorRoles.includes(key))
+             .reduce((obj, key) => {
+                 obj[key] = serverRoles[key];
+                 return obj;
+             }, {});
+
+            var validColorRolesData = {};
+            for(i = 0; i < Object.keys(validColorRoles).length; i++){
+                validColorRolesData[i] = validColorRoles[Object.keys(validColorRoles)[i]];
+            }
+            
+            validColorRolesData = JSON.stringify(validColorRolesData, null, 2);
+
+            fs.writeFileSync('./Roles/colorRoles.json', validColorRolesData , err =>{
+                if(err) throw err;
+            });
+            message.channel.send('Finished writing JSON');
+            
+        })
+    }
+}
--- commands/gacha.js
+++ commands/gacha.js
@@ -1,12 +1,14 @@
+const fs = require('fs');
 const lastTalked = new Set();
 const timeout = new Map();
 const minute = 60000;
 const Threehours = minute*60*3;
+
 module.exports = {
     name: 'gacha',
     description: 'gacha',
     execute(message, args) {
-        
+
         if(lastTalked.has(message.author.id)){
             var currentDate = parseInt(Date.now());
             var lastChat = parseInt(timeout.get(message.author.id));
@@ -19,90 +21,57 @@ module.exports = {
             message.delete(5000);
             return;
         }
-    
+
         lastTalked.add(message.author.id);
         timeout.set(message.author.id, Date.now());
 
-        const member = message.member;
+        fs.readFile('./Roles/colorRoles.json' , (err, data)=>{
+            if(err) throw err;
 
-        const roles = message.guild.roles.map(res => res.name);    
-        const role = Array.from(roles);
-        
-        var arr = [
-            message.guild.roles.find('name' , role[1]),
-            message.guild.roles.find('name' , role[3]),
-            message.guild.roles.find('name' , role[4]),
-            message.guild.roles.find('name' , role[5]),
-            message.guild.roles.find('name' , role[6]),
-            message.guild.roles.find('name' , role[8]),
-            message.guild.roles.find('name' , role[11]),
-            message.guild.roles.find('name' , role[12]),
-            message.guild.roles.find('name' , role[13]),
-            message.guild.roles.find('name' , role[15]),
-            message.guild.roles.find('name' , role[17]),
-            message.guild.roles.find('name' , role[22]),
-            message.guild.roles.find('name' , role[23]),
-            message.guild.roles.find('name' , role[26]),
-            message.guild.roles.find('name' , role[29]),
-            message.guild.roles.find('name' , role[30]),
-            message.guild.roles.find('name' , role[31]),
-            message.guild.roles.find('name' , role[33]),
-            message.guild.roles.find('name' , role[34]),
-            message.guild.roles.find('name' , role[35]),
-            message.guild.roles.find('name' , role[36]),
-            message.guild.roles.find('name' , role[37]),
-            message.guild.roles.find('name' , role[38]),
-        ];
+            const colorRoles = JSON.parse(data);
+            var colorRolesArr = [];
 
+            for(var i = 0; i < Object.keys(colorRoles).length; i++){
+                colorRolesArr[i] = message.guild.roles.find(res => res.name === colorRoles[i]);
+                colorRolesArr.push(colorRolesArr[i]);
+            } 
 
-        if(member.roles.some(res =>[
-            role[1],
-            role[3],
-            role[4],
-            role[5],
-            role[6],
-            role[8],
-            role[11],
-            role[12],
-            role[13],
-            role[15],
-            role[17],
-            role[22],
-            role[23],
-            role[26],
-            role[29],
-            role[30],
-            role[31],
-            role[33],
-            role[34],
-            role[35],
-            role[36],
-            role[37],
-            role[38]
-        ].includes(res.name)) ) {
-        
-        member.removeRoles(member.roles.filter(role => role.name!='Staff' && role.name!='Frequent' && role.name!='Casual' && role.name!='Shush'));
+            fs.readFile('./Roles/memberRoleDB.json' , (err, data)=>{
+                if(err) throw err;
+                var memberDB = JSON.parse(data);
 
-          } else {
-            console.log('oof, no roles');
-          }
-        
+                if(memberDB.hasOwnProperty(message.author.id)){
+                    message.member.removeRole(message.guild.roles.find(res => res.name === memberDB[message.author.id])).then(() =>{
+                        delete memberDB[message.author.id];
+                    });
+                }
 
-        let min = 0; 
-        let max = arr.length;
+                let min = 0; 
+                let max = colorRolesArr.length;
         
-        var randomNum = Math.random() * (max-min) + min;
-        randomNum = Math.floor(randomNum);
+                var randomNum = Math.random() * (max-min) + min;
+                randomNum = Math.floor(randomNum);
+                var colorRole = colorRolesArr[randomNum];
 
-        member.addRole(arr[randomNum]);
-        message.channel.send(`You got the ${arr[randomNum].name} role!`);
-        
-        
+                memberDB[message.author.id] = colorRole.name;
+
+                memberDB = JSON.stringify(memberDB, null, 2);
+                fs.writeFileSync('./Roles/memberRoleDB.json', memberDB, err =>{
+                    if(err) throw err;
+                });
+
+                message.member.addRole(colorRole);
+                message.channel.send(`You got the ${colorRole.name} role!`);
+
+            });
+            
+
+        });
 
         setTimeout(() => {
             lastTalked.delete(message.author.id);
             timeout.delete(message.author.id);
         }, Threehours);
-
+        
     }
 }
