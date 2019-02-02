d6c8d15c19950903756cb2b32622758ff1be49d1
Obert Guo <obertguo@gmail.com>
2019-02-02 15:38:37 -0500

    you can do multiple pings now

--- commands/sleep.js
+++ commands/sleep.js
@@ -1,32 +1,17 @@
-const Discord = require('discord.js');
 module.exports = {
     name: 'sleep',
     description: 'sleep',
     execute(message, args) {
 
-        if(!args[0]){
-            message.channel.send('Input a valid mention or userID');
-            return;
-        }
-        
-        const mention = message.mentions.members.first();
+        let mention = message.mentions.members.map(res => res.user.id);
+        if(mention.length === 0) return message.channel.send('Invalid mention');
 
-        if(!mention){
-            const userID = args[0];
+        for(var i in mention){mention[i] = `<@${mention[i]}>`;}
+        mention = String(mention).replace(',' , ' ');
 
-            message.client.fetchUser(userID)
-            .then(()=>{
-                message.channel.send(`GO SLEEP <@${userID}>!`).then(()=>{
-                    message.delete(1000);
-                });
-                return;
-            }).catch(async ()=>{
-                message.channel.send('Invalid mention or userID');
-            });
-            return;
-        }   
-        message.channel.send(`GO SLEEP <@${mention.id}>!`).then(()=>{
+        message.channel.send(`GO SLEEP ${mention}!`).then((sentMessage)=>{
             message.delete(1000);
+            sentMessage.react(message.client.emojis.get('538193298725011457'));
         });
     }
 }
--- commands/study.js
+++ commands/study.js
@@ -1,32 +1,17 @@
-const Discord = require('discord.js');
 module.exports = {
     name: 'study',
     description: 'study',
     execute(message, args) {
 
-        if(!args[0]){
-            message.channel.send('Input a valid mention or userID');
-            return;
-        }
-        
-        const mention = message.mentions.members.first();
+        let mention = message.mentions.members.map(res => res.user.id);
+        if(mention.length === 0) return message.channel.send('Invalid mention');
 
-        if(!mention){
-            const userID = args[0];
+        for(var i in mention){mention[i] = `<@${mention[i]}>`;}
+        mention = String(mention).replace(',' , ' ');
 
-            message.client.fetchUser(userID)
-            .then(()=>{
-                message.channel.send(`GO STUDY <@${userID}>!`).then(()=>{
-                    message.delete(1000);
-                });
-                return;
-            }).catch(async ()=>{
-                message.channel.send('Invalid mention or userID');
-            });
-            return;
-        }   
-        message.channel.send(`GO STUDY <@${mention.id}>!`).then(()=>{
+        message.channel.send(`GO STUDY ${mention}!`).then((sentMessage)=>{
             message.delete(1000);
+            sentMessage.react('📚');
         });
     }
 }
